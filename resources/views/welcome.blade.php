41854b9a37f49604a94fd61f83bdff1aacc3572b
tsuyoshi-motoyama <motoyama@fix-japan.work>
2021-08-30 23:20:19 +0900

    Revert "commit 04"
    This reverts commit cf295dd5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel03</title>
+        <p>test</p>
+        <p>sarani</p>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
