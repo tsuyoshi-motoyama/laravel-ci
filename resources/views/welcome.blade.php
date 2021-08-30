01bdb40462827f69de39c998e439e90b3a258dfb
tsuyoshi-motoyama <motoyama@fix-japan.work>
2021-08-30 23:18:53 +0900

    Revert "commit 03"
    This reverts commit f691f31a7e293b22930b89fc0633c4d2b220a1d0.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel03</title>
-        <p>test</p>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
